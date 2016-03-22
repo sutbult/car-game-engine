1e22750ef2086058f80cfcd2e6e98a6b2f3ead07
Samuel Utbult <samuel.utbult@hotmail.com>
2016-03-22 15:25:54 +0100

    Idea test start

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,9 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class MainTest extends TestCase {
+    @Test
+    public void testMain() {
+        assertEquals(5, 5);
+    }
+}
---
+++ src/test/java/TestTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestTest {
+    @Test
+    public void testTest() {
+        assertEquals(4, 4);
+    }
+}
