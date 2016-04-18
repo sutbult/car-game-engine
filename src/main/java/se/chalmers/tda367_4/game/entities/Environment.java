419260cf53658c5d92f378749779b151880d03a1
John <exodusx32@gmail.com>
2016-04-18 14:44:42 +0200

    Fix Environment and Obstacle. Refs #13

--- src/main/java/se/chalmers/tda367_4/game/entities/Environment.java
+++ src/main/java/se/chalmers/tda367_4/game/entities/Environment.java
@@ -1,7 +1,27 @@
 package se.chalmers.tda367_4.game.entities;
 
-/**
- * Created by john on 2016-04-18.
- */
-public class Environment {
+import se.chalmers.tda367_4.geometry.*;
+
+public class Environment implements SolidEntity, SingleColorEntity {
+
+    private Triangle triangle;
+    private GraphicalTriangle graphicalTriangle;
+
+    public Environment (Vector2 a, Vector2 b, Vector2 c, float red, float green, float blue) {
+        this(new TriangleImpl(a, b, c), red, green, blue);
+
+    }
+
+    public Environment (Triangle triangle, float red, float green, float blue) {
+        this.triangle = triangle;
+        graphicalTriangle = new GraphicalTriangleImpl(triangle.getCorners(), red, green, blue);
+    }
+
+    public Triangle[] getSolidTriangles() {
+        return new Triangle[] {triangle};
+    }
+
+    public GraphicalTriangle getGraphicalTriangle() {
+        return graphicalTriangle;
+    }
 }
--- src/main/java/se/chalmers/tda367_4/game/entities/Obstacle.java
+++ src/main/java/se/chalmers/tda367_4/game/entities/Obstacle.java
@@ -1,7 +1,16 @@
 package se.chalmers.tda367_4.game.entities;
 
-/**
- * Created by john on 2016-04-14.
- */
-public class Obstacle {
+import se.chalmers.tda367_4.geometry.Triangle;
+import se.chalmers.tda367_4.geometry.Vector2;
+
+public class Obstacle extends Environment {
+
+    public Obstacle (Vector2 a, Vector2 b, Vector2 c, float red, float green, float blue) {
+        super(a, b, c, red, green, blue);
+    }
+
+    public Obstacle (Triangle triangle, float red, float green, float blue) {
+        super(triangle, red, green, blue);
+    }
+
 }
