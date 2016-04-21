6a2ab266580ae38a1272d7749076755654ce7bed
Samuel Utbult <samuel.utbult@hotmail.com>
2016-04-21 13:18:20 +0200

    StartGame rename to Main. Refs #16

--- src/main/java/se/chalmers/tda367_4/game/Main.java
+++ src/main/java/se/chalmers/tda367_4/game/Main.java
@@ -3,7 +3,7 @@ package se.chalmers.tda367_4.game;
 import se.chalmers.tda367_4.scenes.SceneManager;
 import se.chalmers.tda367_4.swingapp.SwingApplication;
 
-public class StartGame {
+public class Main {
 
     public static void main(String[] args){
         SceneManager scene = new SceneManager(new GameApplication());
