47b8ff43bbe6bc845abfcc87840db02c04eab592
Samuel Utbult <samuel.utbult@hotmail.com>
2016-05-19 12:25:45 +0200

    Dummy commit. Refs #48

--- src/main/java/se/chalmers/tda367_4/game/Main.java
+++ src/main/java/se/chalmers/tda367_4/game/Main.java
@@ -4,7 +4,6 @@ import se.chalmers.tda367_4.scenes.SceneManager;
 import se.chalmers.tda367_4.swingapp.SwingApplication;
 
 public class Main {
-
     public static void main(String[] args){
         SceneManager scene = new SceneManager(WorldLoader.createWorld("world1"));
         new SwingApplication(scene);
